79f123b4e9e14a7baf03878620e13cfe0992d6e6
JakobiTalbot <jakobi.talbot@gmail.com>
2019-04-12 13:50:03 +1000

    - Made the main menu work with jakobi scene

---
+++ TestGameJam/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject[] m_players;
+	// Use this for initialization
+	void Awake()
+    {
+        GameObject playerCount = GameObject.FindGameObjectWithTag("PlayerCount");
+        for (int i = 0; i < 4; i++)
+        {
+            // if player isn't playing
+            if (playerCount
+                && !playerCount.GetComponent<PlayerCountSingleton>().m_bPlayersActive[i])
+            {
+                // deactive their object
+                m_players[i].SetActive(false);
+            }
+        }
+	}
+}
